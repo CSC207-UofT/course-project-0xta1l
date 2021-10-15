df69e49a177d60a971d52f5dbc809f65b0be9570
canyildiz02 <canyildiz02@gmail.com>
2021-10-15 15:56:43 -0400

    RecipeSave use case and test

---
+++ src/main/UseCases/RecipeSave.java
@@ -0,0 +1,19 @@
+package UseCases;
+
+import Entities.Recipe;
+import Entities.User;
+
+public class RecipeSave {
+
+public RecipeSave(User user, Recipe recipe) throws Exception {
+   for (Recipe rec: user.getSavedRecipes()) {
+       if (rec.getID() != recipe.getID()){
+           user.addSavedRecipes(recipe);
+       } else{
+           throw new Exception("Recipe is already saved");
+       }
+
+       }
+       }
+
+}
