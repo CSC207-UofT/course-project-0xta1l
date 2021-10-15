f712d23ed30fcb506831c5ba3c267f705a3ef562
Arpi <jamesrp.yang@mail.utoronto.ca>
2021-10-15 01:17:29 -0400

    made small edits, cleaned up code

--- src/main/UseCases/GetRecipe.java
+++ src/main/UseCases/GetRecipe.java
@@ -6,14 +6,13 @@ import Entities.User;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class GetRecipe {
     public HashMap<Integer, ArrayList<Object>> getUserSavedRecipes(User user) {
         ArrayList<Recipe> userRecipeList = user.getSavedRecipes();
 
         GenreLibrary userLibrary = new GenreLibrary();
-        HashMap<Integer, ArrayList<Object>> recipeMap = new HashMap<Integer, ArrayList<Object>>();
+        HashMap<Integer, ArrayList<Object>> recipeMap = new HashMap<>();
 
         for (Recipe recipe: userRecipeList) {
             Recipe variable = userLibrary.getRecipeByID("All", recipe.getID());
@@ -24,13 +23,13 @@ public class GetRecipe {
     }
 
     public ArrayList<Object> getSingleRecipe (int recipeID, String fullOrPreview) throws Exception {
-        ArrayList<String> validOptionsFull = new ArrayList<String>();
+        ArrayList<String> validOptionsFull = new ArrayList<>();
         validOptionsFull.add("Full");
         validOptionsFull.add("full");
         validOptionsFull.add("f");
         validOptionsFull.add("F");
 
-        ArrayList<String> validOptionsPreview = new ArrayList<String>();
+        ArrayList<String> validOptionsPreview = new ArrayList<>();
         validOptionsPreview.add("Preview");
         validOptionsPreview.add("preview");
         validOptionsPreview.add("prev");
@@ -38,7 +37,7 @@ public class GetRecipe {
         validOptionsPreview.add("P");
 
         GenreLibrary userLibrary = new GenreLibrary();
-        ArrayList<Object> recipeProperties = new ArrayList<Object>();
+        ArrayList<Object> recipeProperties = new ArrayList<>();
 
         if (!validOptionsPreview.contains(fullOrPreview)
                 && !validOptionsFull.contains(fullOrPreview)) {
@@ -57,7 +56,7 @@ public class GetRecipe {
     public ArrayList<ArrayList<Object>> getGenreRecipes (String genreName) {
         GenreLibrary genreLibrary = new GenreLibrary();
 
-        HashMap<Integer, Recipe> genre = new HashMap<Integer, Recipe>();
+        HashMap<Integer, Recipe> genre;
         genre = genreLibrary.getAllRecipes(genreName);
 
         ArrayList<ArrayList<Object>> previewList = new ArrayList<>();
