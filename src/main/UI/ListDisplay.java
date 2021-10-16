4e76f496a74ec9a36fb4ce4dd201e9d405f2a961
Amir <amir.alleyne@mail.utoronto.ca>
2021-10-16 13:09:10 -0400

    Added UI ViewDisplay

--- src/main/UI/ListDisplay.java
+++ src/main/UI/ListDisplay.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class ListDisplay {
-    public void Display(ArrayList<Object> browseInfo) {
+    public void lDisplay(ArrayList<Object> browseInfo) {
         if (browseInfo.get(1).equals("Genre")) {
             for (String genre : browseInfo.get(0)) {
                 System.out.println(genre + "\n");
