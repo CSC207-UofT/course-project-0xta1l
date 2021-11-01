6f2bef939a89a783478fad2f85f19100cf04caa5
kalleynebb <kaialleynebds@gmail.com>
2021-11-01 13:23:09 -0400

    GetReview Use Case implemented. getRecipeReviews method does not use HashMap as was suggested in CRC code. getUserReviews is completed. getSingle not implemented.Currently some repeating code I'm not sure how to refactor. Also, any insight on the add error?

---
+++ src/main/UseCases/GetReview.java
@@ -0,0 +1,51 @@
+package UseCases;
+
+import Entities.*;
+import Constants.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GetReview {
+
+    public ArrayList<ArrayList<Object>> getRecipeReviews(int recipeID){
+        // Implemented differently to CRC card. No use of hash map because Recipe.getRecipeReviews() returns
+        // an ArrayList
+
+        GenreLibrary genreLibrary = new GenreLibrary();
+        Recipe specificRecipe = genreLibrary.getRecipeByID("All", recipeID);
+        ArrayList<Review> reviewList = specificRecipe.getRecipeReviews();
+        // HashMap<User, Review> reviewMap = specificRecipe.getRecipeReviews();
+
+        ArrayList<ArrayList<Object>> finalReviewList = new ArrayList<>();
+        for (Review review: reviewList){
+            int rating = review.getRating();
+            String userID = review.getUserID();
+            String comment = review.getComments();
+            finalReviewList.add(rating,comment,userID);
+        }
+        return finalReviewList;
+    }
+
+    public ArrayList<ArrayList<Object>> getUserReviews(String username){
+        UserSecurity userSecurity = new UserSecurity();
+        HashMap<String, User> userMap = userSecurity.getUsernames();
+        User specificUser = userMap.get(username);
+        ArrayList<Review> reviewList = specificUser.getUserReviews();
+
+        ArrayList<ArrayList<Object>> finalReviewList = new ArrayList<>();
+        for (Review review: reviewList){
+            int rating = review.getRating();
+            String recipeID = review.getRecipeID();
+            String comment = review.getComments();
+            finalReviewList.add(rating,comment,recipeID);
+        }
+        return finalReviewList;
+    }
+
+    public Review getSingle(String reviewID){
+
+    }
+
+
+
+}
