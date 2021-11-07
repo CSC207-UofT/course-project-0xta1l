47029b97b7d6201d86b19b23b650f5fccb5cce4e
Amir <amir.alleyne@mail.utoronto.ca>
2021-11-07 13:41:25 -0500

    Added Review Controller

--- src/main/Controllers/UserRequestCreateReview.java
+++ src/main/Controllers/UserRequestCreateReview.java
@@ -3,12 +3,12 @@ package Controllers;
 import UseCases.GetReview;
 
 public class UserRequestReview {
-    public boolean reviewRecipe(String userID, String recipeID, String reviewData) throws Exception{
-        if (!(1<= reviewData.rating <= 5)){ // what will be the format on reviewData?
+    public boolean reviewRecipe(String userID, String recipeID, String comment, int rating) throws Exception{
+        if (rating > 5) or (rating < 1){ // what will be the format on reviewData?
             throw new Exception("Data is incomplete!");
         }
-        Boolean reviewAdded = RecipeReviewAdd.addReview(userID, recipeID, reviewData); // useCase was not implemented prior; should i implement it?
-        UserInputStatus(reviewAdded); // this is not created; will this be a thing?
+        Boolean reviewAdded = RecipeReviewAdd.addReview(userID, recipeID, comment, rating); // useCase was not implemented prior; should i implement it?
+
         if (reviewAdded){
             return true;
         }
