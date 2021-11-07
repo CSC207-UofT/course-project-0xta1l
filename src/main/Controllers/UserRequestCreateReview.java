a58fcaaa3217c781a73a3a7d397b99dd05f25835
Amir <amir.alleyne@mail.utoronto.ca>
2021-11-07 14:33:22 -0500

    Added Review Usecase, constructor and editted the temp_data csv file to categorize genres. Lingering questions were commented in the UserRequestCreateReview file

--- src/main/Controllers/UserRequestCreateReview.java
+++ src/main/Controllers/UserRequestCreateReview.java
@@ -1,18 +1,18 @@
 package Controllers;
 
+import Entities.Review;
 import UseCases.GetReview;
+import UseCases.RecipeReviewAdd;
 
-public class UserRequestReview {
-    public boolean reviewRecipe(String userID, String recipeID, String comment, int rating) throws Exception{
-        if (rating > 5) or (rating < 1){ // what will be the format on reviewData?
+public class UserRequestCreateReview {
+    public Review reviewRecipe(String userID, String recipeID, String comment, int rating) throws Exception{
+        if ((rating > 5) || (rating < 1)){
             throw new Exception("Data is incomplete!");
         }
-        Boolean reviewAdded = RecipeReviewAdd.addReview(userID, recipeID, comment, rating); // useCase was not implemented prior; should i implement it?
-
-        if (reviewAdded){
-            return true;
-        }
-        return false;
+        Review review = new Review(userID, recipeID, comment,rating);// should this review be added to a csv file?
+        RecipeReviewAdd recipeAdd = new RecipeReviewAdd();
+        recipeAdd.addReview(review);
+        return review;
     }
 
 }
