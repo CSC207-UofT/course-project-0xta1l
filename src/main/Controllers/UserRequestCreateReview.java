6743e3b294e5b9e351c03ebcfd8cb6378f79ba85
Amir <amir.alleyne@mail.utoronto.ca>
2021-11-08 13:12:01 -0500

    Changed and refractored types of Review and Recipe Variables. Added methods into use cases and controllers relating to Reviews.

--- src/main/Controllers/UserRequestCreateReview.java
+++ src/main/Controllers/UserRequestCreateReview.java
@@ -4,14 +4,13 @@ import Entities.Review;
 import UseCases.GetReview;
 import UseCases.RecipeReviewAdd;
 
-public class UserRequestCreateReview {
-    public Review reviewRecipe(String userID, String recipeID, String comment, int rating) throws Exception{
+public class UserRequestCreateReview { // database should be called here to write recipe to database
+    public Review reviewRecipe(String userID, int recipeID, String comment, int rating) throws Exception{
         if ((rating > 5) || (rating < 1)){
             throw new Exception("Data is incomplete!");
         }
-        Review review = new Review(userID, recipeID, comment,rating);// should this review be added to a csv file?
         RecipeReviewAdd recipeAdd = new RecipeReviewAdd();
-        recipeAdd.addReview(review);
+        Review review =  recipeAdd.addReview(userID, recipeID, comment, rating);
         return review;
     }
 
