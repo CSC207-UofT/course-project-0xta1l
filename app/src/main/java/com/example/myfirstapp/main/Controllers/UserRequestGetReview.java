d9fad5c3a92be37db14daa0ea007f2a1c6e58304
mieko <miekoyao@gmail.com>
2021-11-14 02:40:34 -0500

    fixed some more pathing

--- app/src/main/java/com/example/myfirstapp/main/Controllers/UserRequestGetReview.java
+++ app/src/main/java/com/example/myfirstapp/main/Controllers/UserRequestGetReview.java
@@ -1,18 +1,18 @@
 package com.example.myfirstapp.main.Controllers;
 
-import com.example.myfirstapp.main.Entities.Review;
+import com.example.myfirstapp.main.UseCases.GetReview;
 
 import java.util.ArrayList;
 
 public class UserRequestGetReview {
     public ArrayList<ArrayList<Object>> getrecipereview(String recipeID){
         int ID = Integer.parseInt(recipeID);
-        UseCases.GetReview getrecreview = new UseCases.GetReview();
+        GetReview getrecreview = new GetReview();
         return getrecreview.getRecipeReviews(ID);
     }
 
     public ArrayList<ArrayList<Object>> getuserreview(String username)   {
-            UseCases.GetReview getusereview= new UseCases.GetReview();
+            GetReview getusereview= new GetReview();
             return getusereview.getUserReviews(username);
 
         }
