81a87894d10d10351704faf22a13c133852e76f0
Justin Zheng <83163126+justinzzy-code@users.noreply.github.com>
2021-11-15 13:08:15 -0500

    Add files via upload
    changed test file directory

---
+++ app/src/test/java/test/UserCreateTest.java
@@ -0,0 +1,38 @@
+/*
+package com.example.myfirstapp.test;
+
+import com.example.myfirstapp.main.Entities.User;
+import com.example.myfirstapp.main.Entities.UserSecurity;
+import com.example.myfirstapp.main.UseCases.UserCreate;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class UserCreateTest {
+
+
+    @Test(timeout = 50)
+    public void TestUserCreate1() {
+        UserCreate usercreate = new UserCreate();
+        ArrayList<String> interests = new ArrayList<>();
+        interests.add("dessert");
+        interests.add("chinese");
+        boolean value = usercreate.userCreate("Jo", 19, "123",
+                "username1", "hi", interests);
+        assertFalse(value);
+    }
+
+    @Test(timeout = 50)
+    public void TestUserCreate2() {
+        UserCreate usercreate = new UserCreate();
+        ArrayList<String> interests = new ArrayList<>();
+        interests.add("dessert");
+        interests.add("chinese");
+        boolean value = usercreate.userCreate("Jo", 19, "123", "jj",
+                "hi", interests);
+        assertTrue(value);
+    }
+}
+*/
