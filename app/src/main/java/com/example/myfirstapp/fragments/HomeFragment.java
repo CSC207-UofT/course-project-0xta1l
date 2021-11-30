49b750544af5b8c90fb3d6455c3547016394842f
Jolina Li <jolina.li@mail.utoronto.ca>
2021-11-29 21:25:39 -0500

    coded HomeFragment.java (recommendation system)

--- app/src/main/java/com/example/myfirstapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/myfirstapp/fragments/HomeFragment.java
@@ -1,14 +1,40 @@
 package com.example.myfirstapp.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.myfirstapp.Globals;
 import com.example.myfirstapp.R;
+import com.example.myfirstapp.genreActivity.GenreItemActivity;
+import com.example.myfirstapp.genreActivity.GenreRecipeItemActivity;
+import com.example.myfirstapp.main.Constants.Constants;
+import com.example.myfirstapp.main.Controllers.UserRequestBrowse;
+import com.example.myfirstapp.main.Controllers.UserRequestRecommend;
+import com.example.myfirstapp.main.Controllers.UserRequestSort;
+import com.example.myfirstapp.main.Entities.Preview;
+import com.example.myfirstapp.main.Entities.Recipe;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -48,19 +74,126 @@ public class HomeFragment extends Fragment {
         return fragment;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        UserRequestRecommend r = new UserRequestRecommend();
+        String username = Globals.getUser_username();
+        ArrayList<Preview> lst = r.recommendRecipes(username, 1);
+        showItems(lst);
+
+        return v;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void showItems(ArrayList<Preview> recipes) {
+        LinearLayout layout = (LinearLayout) requireView().findViewById(R.id.RecommendLayout);
+        layout.removeAllViews();
+        for(Preview recipePreview: recipes) {
+            RelativeLayout p = this.createRecipePreview(recipePreview);
+            layout.addView(p);
+        }
     }
+
+
+    public TextView createRecipeName(Preview recipePreview, int height) {
+        TextView text = new TextView(getContext());
+        text.setGravity(Gravity.CENTER);
+        text.setTextColor(getResources().getColor(android.R.color.black));
+        text.setPadding(40, 40 + height, 40, 0);
+        text.setTextSize(24);
+        text.setGravity(Gravity.LEFT);
+        String recipeName = recipePreview.getName();
+        text.setText(recipeName);
+        text.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+        text.setClickable(true);
+        int id = recipePreview.getID();
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getContext();
+                Globals.setViewRecipeId(id);
+                Intent intent = new Intent(context, GenreRecipeItemActivity.class);
+                startActivity(intent);
+            }
+        });
+        return text;
+    }
+    public TextView createRecipeDetail(Preview recipePreview, int height) {
+        TextView text = new TextView(getContext());
+        text.setGravity(Gravity.CENTER);
+        text.setTextColor(getResources().getColor(android.R.color.black));
+        text.setPadding(100, 120 + height, 40, 40);
+        text.setTextSize(18);
+        text.setGravity(Gravity.LEFT);
+        String recipeDescription = recipePreview.getDescription();
+        text.setText(recipeDescription);
+        text.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
+        text.setClickable(true);
+        int id = recipePreview.getID();
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getContext();
+                Globals.setViewRecipeId(id);
+                Intent intent = new Intent(context, GenreRecipeItemActivity.class);
+                startActivity(intent);
+            }
+        });
+        return text;
+    }
+    public ImageView createRecipeImage(Preview recipePreview) {
+        ImageView text = new ImageView(getContext());
+        int id = recipePreview.getID();
+        String imgName = "img_" + String.valueOf(id);
+        text.setImageResource(getResources().getIdentifier(imgName, "drawable", requireActivity().getPackageName()));
+        text.setPadding(10, 20, 0, 0);
+        text.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        text.setClickable(true);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getContext();
+                Globals.setViewRecipeId(id);
+                Intent intent = new Intent(context, GenreRecipeItemActivity.class);
+                startActivity(intent);
+            }
+        });
+        return text;
+    }
+    public int getImageHeight(Preview recipePreview) {
+        ImageView text = new ImageView(getContext());
+        text.setAdjustViewBounds(true);
+        int id = recipePreview.getID();
+        String imgName = "img_" + String.valueOf(id);
+        text.setImageResource(getResources().getIdentifier(imgName, "drawable", requireActivity().getPackageName()));
+        text.setPadding(10, 20, 0, 0);
+        text.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        int h = text.getDrawable().getIntrinsicHeight();
+        return h;
+    }
+    public RelativeLayout createRecipePreview(Preview recipe) {
+        RelativeLayout p = new RelativeLayout(getContext());
+        p.addView(this.createRecipeImage(recipe));
+        int height = this.getImageHeight(recipe);
+        p.addView(this.createRecipeName(recipe, height));
+        p.addView(this.createRecipeDetail(recipe, height));
+        return p;
+    }
+
+
+
 }
