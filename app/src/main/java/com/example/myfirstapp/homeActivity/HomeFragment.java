6185e80ce1d9c9e783397c5e8c1ba5c34fc0de5c
Justin Zheng <83163126+justinzzy-code@users.noreply.github.com>
2021-12-05 12:12:41 -0500

    Update HomeFragment.java

--- app/src/main/java/com/example/myfirstapp/homeActivity/HomeFragment.java
+++ app/src/main/java/com/example/myfirstapp/homeActivity/HomeFragment.java
@@ -1,5 +1,5 @@
 package com.example.myfirstapp.homeActivity;
-
+import android.graphics.Typeface;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -82,6 +82,7 @@ public class HomeFragment extends Fragment {
         text.setTextSize(26);
         text.setGravity(Gravity.CENTER);
         text.setTextColor(Color.rgb(22,83,126));
+        text.setTypeface(null, Typeface.BOLD);
         String recipeName = recipePreview.getName();
         text.setText(recipeName);
         text.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT));
@@ -102,7 +103,7 @@ public class HomeFragment extends Fragment {
         TextView text = new TextView(getContext());
         text.setGravity(Gravity.CENTER);
         text.setTextColor(getResources().getColor(android.R.color.black));
-        text.setPadding(100, 120 + height, 40, 40);
+        text.setPadding(100, 200 + height, 40, 40);
         text.setTextSize(20);
         text.setGravity(Gravity.LEFT);
         text.setTextColor(Color.rgb(22,83,126));
