135a835460297b5a2c8012b3ae0b119d32bde7f1
Arpi <jamesrp.yang@mail.utoronto.ca>
2021-12-05 12:29:28 -0500

    added code from Test App for ReadRecipe, ReadReview, and ReadUser

--- app/src/main/java/com/example/myfirstapp/main/Gateways/ReadRecipe.java
+++ app/src/main/java/com/example/myfirstapp/main/Gateways/ReadRecipe.java
@@ -6,5 +6,64 @@ import com.google.firebase.database.DataSnapshot;
 import java.util.ArrayList;
 
 public class ReadRecipe {
-    // TODO: fill in this class from Test App code
+
+    public static Recipe readRecipe(DataSnapshot singleRecipeRef) {
+        // Read recipe attributes from singleRecipeRef
+        int recipeID = singleRecipeRef.child("id").getValue(Integer.class);
+        String recipeDescription = singleRecipeRef.child("description").getValue(String.class);
+        ArrayList<String> recipeGenres = new ArrayList<>();
+        DataSnapshot recipeGenreListDatabase = singleRecipeRef.child("genre");
+        for (DataSnapshot genre : recipeGenreListDatabase.getChildren()) {
+            recipeGenres.add(genre.getValue(String.class));
+        }
+        String recipeImage = singleRecipeRef.child("image").getValue(String.class);
+        String recipeIngredients = singleRecipeRef.child("ingredients").getValue(String.class);
+        String recipeInstructions = singleRecipeRef.child("instructions").getValue(String.class);
+        String recipeName = singleRecipeRef.child("name").getValue(String.class);
+        int recipePreptime = recipePrepReader(singleRecipeRef);
+        int recipeRating = recipeRatingReader(singleRecipeRef);
+
+        // Construct and return a new recipe
+        return new Recipe(recipeInstructions, recipeIngredients,
+                recipeGenres, recipeName, recipeRating, recipeID, recipeImage,
+                recipeDescription, recipePreptime);
+    }
+
+    private static int recipePrepReader(DataSnapshot singleRecipeRef) {
+
+        // Check if the recipe from the database has a valid prep time attribute.
+        // If it does, return the prep time attribute value.
+        if (singleRecipeRef.child("prep time").exists()) {
+            Object recipePrepHolder = singleRecipeRef.child("preptime").getValue();
+
+            if (recipePrepHolder instanceof Integer) {
+                return (Integer) recipePrepHolder;
+
+            }
+        }
+
+        // If no eligible value for prep time exists, return default value of 60 minutes.
+        return 60;
+
+
+    }
+
+    private static int recipeRatingReader(DataSnapshot singleRecipeRef) {
+
+        // Check if the recipe from the database has a valid rating attribute.
+        // If it does, return the rating attribute value.
+        if (singleRecipeRef.child("rating").exists()) {
+            Object recipeRatingHolder = singleRecipeRef.child("rating").getValue();
+
+            if (recipeRatingHolder instanceof Integer) {
+                return (Integer) recipeRatingHolder;
+
+            }
+        }
+
+        // If no eligible value for rating exists, return default value of 3.
+        return 3;
+
+
+    }
 }
